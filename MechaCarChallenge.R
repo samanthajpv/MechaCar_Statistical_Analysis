64f5752bbda7ef67fbdb8fe4477e3d858e7ed668
Samantha Villanueva <samanthavillanueva@Samanthas-MacBook-Pro.local>
2021-09-20 22:40:01 -0700

    add PSI script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,7 @@ library(dplyr) #load dplyr
 MechaCar_mpg <- read.csv('MechaCar_mpg.csv', check.names=F, stringsAsFactors=F) #load data
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)) #generate summary statistics
+
+# Deliverable 2
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') #PSI statistics
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') #Lot statistics
